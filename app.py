c89ad4435993f49cafc559256225d464e3ccc49d
Lei Zean <lei.zean.ernest.nemeno@aclcbutuan.edu.ph>
2025-02-12 11:18:01 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,82 @@
+print("BootCamp Activity 2 \n")
+
+
+print("_______________")
+print("CHATBot")
+print("_______________\n")
+
+name = input("Hello My Name Is CHATBot!, What Is Your Name? ")
+print(f"Hello {name.capitalize()}!, Nice To Meet You!")
+age = int(input(f"How Old Are You {name.capitalize()}? "))
+print(f"Wow!, So You Are {age} Years Old!")
+food = input(f"So {name.capitalize()} Tell Me, What is Your Favorite Food? ")
+print(f"Now Great!, So Right Now I Will Print All Of Your Answer.")
+print("\n")
+print("\n")
+print("\n")
+
+print(f"Name: {name.capitalize()}")
+print("__________________________")
+print(f"Age: {age}")
+print("__________________________")
+print(f"Fav Food: {food.upper()}")
+print("__________________________\n")
+
+
+print(f"First Letter Of Your Name: {name[1]}")
+print(f"Reverse Letter Of Your Name: {name[::-1]}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
